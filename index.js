4e6eaf69cb95d31620bd63c2cada5d8ce3bf7f32
Nerevakiin <djmatisk@yahoo.gr>
2025-09-13 16:20:12 +0300

    querySelector update

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import {catsData} from './data.js'
 
+
+//Create an empty array and fill it with the data imported 
 function getEmotionsArray(cats){
     const emotionsArray = []
     for (let cat of cats){
@@ -12,15 +14,36 @@ function getEmotionsArray(cats){
     return emotionsArray 
 }
 
-const emotionRadios = document.getElementById("emotion-radios")
 
+//Make the 'get image' btn functional by grabbin the id of the selected radio
+
+const getImageBtn = document.getElementById('get-image-btn')
+getImageBtn.addEventListener('click', getMatchingCatsArray)
+
+function getMatchingCatsArray(e){
+    if (document.querySelector('input[type=radio]:checked')){
+        const selectedEmotion = document.querySelector('input[type=radio]:checked').value
+        console.log(selectedEmotion)
+    }
+}
+
+
+//UI in select/deselect radio choice
+const emotionRadios = document.getElementById("emotion-radios")
 emotionRadios.addEventListener("change", highlightCheckedOption)
     
-
 function highlightCheckedOption(e){
+
+    const radiosArray = document.getElementsByClassName('radio')
+    for (let radios of radiosArray) {
+        radios.classList.remove("highlight")
+    }
+    
     document.getElementById(e.target.id).parentElement.classList.add("highlight")
 }
 
+
+//Render the emotion choices on the DOM
 function renderEmotionsRadios(cats){
     const emotions = getEmotionsArray(cats)
     let emotionsList = " "
